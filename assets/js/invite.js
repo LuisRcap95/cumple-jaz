c724a3355491140545450c8d87e7ee8ddf416a96
LuisRcap95 <luisrcap95@gmail.com>
2024-08-28 22:58:03 -0600

    Hora agregada :fire:

--- assets/js/invite.js
+++ assets/js/invite.js
@@ -34,7 +34,7 @@ video.addEventListener('timeupdate', function(e) {
 const cuteTexts = [
     `<div class="slider_caption">
         <span class="span-medieval">Lady y/o Sir Caballero. Se le hace la cordial invitación para asistir al
-        cumpleaños de Lady Jaz el día sábado 5 de octubre para celebrar sus 25 primaveras. 
+        cumpleaños de Lady Jaz el día sábado 5 de octubre a las 19:00 para celebrar sus 25 primaveras. 
         Esperando su participación para el gran evento en la siguiente dirección:<br>
         <a href="https://maps.app.goo.gl/fCu4U66y1sVXphgn8">Av. Girasol 1, Las Huertas
         1ra Secc, 53427 Naucalpan de Juárez, Méx.</a><br>
@@ -123,6 +123,7 @@ function addFormListener() {
         btnSubmit.classList.remove('pap-btn');
         btnSubmit.classList.add('btn');
         btnSubmit.classList.add('btn-success');
+        btnSubmit.disabled = true
 
         const successMessage = document.createElement('span');
         successMessage.innerHTML = `Estamos registrando sus datos`;
